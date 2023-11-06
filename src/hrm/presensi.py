436cc0d3b244f1c1b4cbc89823602b033d8b6154
odhyp <pradhana.odhy@gmail.com>
2023-11-06 16:14:08 +0700

    Add login and page navigation

--- src/hrm/presensi.py
+++ src/hrm/presensi.py
@@ -1,4 +1,9 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+
+import time
 
 
 class WebDriver:
@@ -13,5 +18,24 @@ class WebDriver:
 
 
 with WebDriver(webdriver.Chrome()) as wd:
+    wd.maximize_window()
     wd.get('https://presensi2.jogjaprov.go.id/')
-    print(wd.page_source)
+
+    username = wd.find_element(By.NAME, "username")
+    password = wd.find_element(By.NAME, "password")
+    username.send_keys("197708151998032002")
+    password.send_keys("Feri2022#")
+    password.send_keys(Keys.RETURN)
+    wd.get("https://presensi2.jogjaprov.go.id/lap-pres/rekap/?menu_id=15")
+    tanggal_mulai = wd.find_element(By.NAME, "tanggal_mulai")
+    tanggal_selesai = wd.find_element(By.NAME, "tanggal_selesai")
+    tanggal_mulai.send_keys("2023-10-01")
+    tanggal_selesai.send_keys("2023-10-31")
+    find_export_mode = wd.find_element(By.ID, 'fatih')
+    select_export_mode = Select(find_export_mode)
+    select_export_mode.select_by_value("excel")
+
+    submit = wd.find_element(By.XPATH, '//*[@id="ahsan"]/div[3]/button')
+    submit.click()
+
+    time.sleep(10)
