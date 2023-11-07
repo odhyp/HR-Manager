264b7d6818fda1dd54bb16abacc0249f600ed7a9
odhyp <pradhana.odhy@gmail.com>
2023-11-07 11:59:32 +0700

    Add login method

--- src/hrm/driver_bot.py
+++ src/hrm/driver_bot.py
@@ -1,4 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
 
 
 class DriverBot:
@@ -9,3 +12,22 @@ class DriverBot:
 
     def quit(self):
         self.bot.quit()
+
+    def login_presensi(self):
+        bot = self.bot
+        bot.maximize_window()
+        bot.get('https://presensi2.jogjaprov.go.id/')
+
+        time.sleep(1)
+
+        username = bot.find_element(By.NAME, 'username')
+        password = bot.find_element(By.NAME, 'password')
+        username.clear()
+        password.clear()
+        username.send_keys(self.username)
+        password.send_keys(self.password)
+        password.send_keys(Keys.RETURN)
+
+        time.sleep(1)
+
+        print("Login success")
