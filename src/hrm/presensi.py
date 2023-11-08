fa760725fe158f88a83dc11a17dd00ea31f97bfa
odhyp <pradhana.odhy@gmail.com>
2023-11-08 22:01:17 +0700

    Remove testing code

--- src/hrm/presensi.py
+++ src/hrm/presensi.py
@@ -15,27 +15,3 @@ class WebDriver:
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.driver.quit()
-
-
-with WebDriver(webdriver.Chrome()) as wd:
-    wd.maximize_window()
-    wd.get('https://presensi2.jogjaprov.go.id/')
-
-    username = wd.find_element(By.NAME, "username")
-    password = wd.find_element(By.NAME, "password")
-    username.send_keys("197708151998032002")
-    password.send_keys("Feri2022#")
-    password.send_keys(Keys.RETURN)
-    wd.get("https://presensi2.jogjaprov.go.id/lap-pres/rekap/?menu_id=15")
-    tanggal_mulai = wd.find_element(By.NAME, "tanggal_mulai")
-    tanggal_selesai = wd.find_element(By.NAME, "tanggal_selesai")
-    tanggal_mulai.send_keys("2023-10-01")
-    tanggal_selesai.send_keys("2023-10-31")
-    find_export_mode = wd.find_element(By.ID, 'fatih')
-    select_export_mode = Select(find_export_mode)
-    select_export_mode.select_by_value("excel")
-
-    submit = wd.find_element(By.XPATH, '//*[@id="ahsan"]/div[3]/button')
-    submit.click()
-
-    time.sleep(10)
