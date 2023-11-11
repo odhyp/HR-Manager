ad05abc816071b64320f19cd2218012c92654ccf
odhyp <pradhana.odhy@gmail.com>
2023-11-11 10:40:22 +0700

    Add getters for admin username and password

--- src/hrm/utils.py
+++ src/hrm/utils.py
@@ -11,3 +11,15 @@ def get_password_path():
     root_path = get_root_path()
     password_path = Path(root_path, 'password.txt')
     return password_path
+
+
+def get_admin_username():
+    with open(get_password_path()) as f:
+        username = f.readlines()[0].strip()
+        return str(username)
+
+
+def get_admin_password():
+    with open(get_password_path()) as f:
+        password = f.readlines()[1].strip()
+        return str(password)
