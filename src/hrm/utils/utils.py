98ecbad8c2b3c373c0d00278b421ed710cddae5a
odhyp <pradhana.odhy@gmail.com>
2023-11-12 17:42:46 +0700

    Fix get_root_path after project restructure

--- src/hrm/utils/utils.py
+++ src/hrm/utils/utils.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 def get_root_path():
     current_path = Path().absolute()
-    root_path = Path(current_path).parents[1]
+    root_path = Path(current_path).parents[2]
     return root_path
 
 
