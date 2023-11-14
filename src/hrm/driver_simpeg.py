e03dc4e8d8113e1c470b4df58e4bed4b3cff5b10
odhyp <pradhana.odhy@gmail.com>
2023-11-14 10:22:03 +0700

    Add try-except block to handle wait method

--- src/hrm/driver_simpeg.py
+++ src/hrm/driver_simpeg.py
@@ -3,6 +3,8 @@ from src.hrm.driver_base import DriverBase
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
 
 simpeg = "https://simpeg2.jogjaprov.go.id/prod/"
 
@@ -16,6 +18,12 @@ class DriverSimpeg(DriverBase):
     def login(self):
         driver = self.driver
         driver.get(simpeg)
+
+        try:
+            self.wait().until(EC.presence_of_element_located((By.NAME, "username")))
+        except NoSuchElementException:
+            print(NoSuchElementException)
+            self.quit()
 
         form_username = driver.find_element(By.NAME, 'username')
         form_password = driver.find_element(By.NAME, 'password')
