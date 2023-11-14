f851faa7a83b86fc60f419f6c7cd7b40b7276dde
odhyp <pradhana.odhy@gmail.com>
2023-11-14 10:33:43 +0700

    Rewrite DriverPresensi using DriverBase superclass

--- src/hrm/driver_presensi.py
+++ src/hrm/driver_presensi.py
@@ -1,97 +1,33 @@
-from selenium import webdriver
+from src.hrm.driver_base import DriverBase
+
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
-from selenium.webdriver.support.wait import WebDriverWait
+
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
 
-from utils import get_password_path
+presensi = "https://presensi2.jogjaprov.go.id/"
 
 
-class DriverPresensi:
-    def __init__(self):
-        self.bot = webdriver.Chrome()
-
-    def get_username(self):
-        with open(get_password_path()) as f:
-            username = f.readlines()[0].strip()
-            return str(username)
-
-    def get_password(self):
-        with open(get_password_path()) as f:
-            password = f.readlines()[1].strip()
-            return str(password)
-
-    def quit(self):
-        self.bot.quit()
-
-    def wait(self):
-        return WebDriverWait(self.bot, 10)
+class DriverPresensi(DriverBase):
+    def __init__(self, username, password):
+        super().__init__()
+        self.username = username
+        self.password = password
 
     def login(self):
-        username = self.get_username()
-        password = self.get_password()
-        bot = self.bot
-        bot.maximize_window()
-        bot.get('https://presensi2.jogjaprov.go.id/')
+        driver = self.driver
+        driver.get(presensi)
 
         try:
-            self.wait().until(EC.presence_of_element_located((By.CLASS_NAME, "form-group")))
+            self.wait().until(EC.presence_of_element_located((By.NAME, 'username')))
         except NoSuchElementException:
             print(NoSuchElementException)
             self.quit()
 
-        # Elements
-        username_form = bot.find_element(By.NAME, 'username')
-        password_form = bot.find_element(By.NAME, 'password')
+        form_username = driver.find_element(By.NAME, 'username')
+        form_password = driver.find_element(By.NAME, 'password')
 
-        # Actions
-        username_form.clear()
-        password_form.clear()
-        username_form.send_keys(username)
-        password_form.send_keys(password)
-        password_form.send_keys(Keys.RETURN)
-
-        try:
-            self.wait().until(EC.presence_of_element_located((By.ID, "mainnav-menu")))
-            print("found mainnav-menu")
-        except NoSuchElementException:
-            print(NoSuchElementException)
-            self.quit()
-
-        print("Login success")
-
-    def rekap_presensi(self):
-        bot = self.bot
-        bot.get("https://presensi2.jogjaprov.go.id/lap-pres/rekap/?menu_id=15")
-
-        # Elements
-        tanggal_mulai = bot.find_element(By.NAME, "tanggal_mulai")
-        tanggal_selesai = bot.find_element(By.NAME, "tanggal_selesai")
-        export_mode = bot.find_element(By.ID, 'fatih')
-        submit = bot.find_element(By.XPATH, '//*[@id="ahsan"]/div[3]/button')
-
-        # Actions
-        tanggal_mulai.send_keys("2023-11-01")
-        tanggal_selesai.send_keys("2023-11-07")
-        Select(export_mode).select_by_value("excel")
-        submit.click()
-
-        print("File downloaded")
-
-    def rekap_prestasi(self):
-        bot = self.bot
-        bot.get("https://presensi2.jogjaprov.go.id/lap-pres/prestasi/?menu_id=16")
-
-        # Elements
-        bulan = bot.find_element(By.NAME, "bulan")
-        export_mode = bot.find_element(By.ID, 'fatih')
-        submit = bot.find_element(By.XPATH, '//*[@id="ahsan"]/div[3]/button')
-
-        # Actions
-        bulan.send_keys("11/2023")
-        Select(export_mode).select_by_value("excel")
-        submit.click()
-
-        print("File downloaded")
+        form_username.send_keys(self.username)
+        form_password.send_keys(self.password)
+        form_password.send_keys(Keys.RETURN)
