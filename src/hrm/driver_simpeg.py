78af88e3d19da106eb12257536152ea374582b24
odhyp <pradhana.odhy@gmail.com>
2023-11-14 10:14:11 +0700

    Fix import path

--- src/hrm/driver_simpeg.py
+++ src/hrm/driver_simpeg.py
@@ -1,8 +1,8 @@
-from driver_base import DriverBase
+from src.hrm.driver_base import DriverBase
+
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-import time
-from utils import get_username, get_password
+
 
 simpeg = "https://simpeg2.jogjaprov.go.id/prod/"
 
@@ -25,7 +25,3 @@ class DriverSimpeg(DriverBase):
         form_username.send_keys(username)
         form_password.send_keys(password)
         form_password.send_keys(Keys.RETURN)
-
-        time.sleep(5)
-
-        driver.quit()
