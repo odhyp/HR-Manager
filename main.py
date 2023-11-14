a8e1ed6f26e39b5cd8c2a1dbdffda7ad106250d1
odhyp <pradhana.odhy@gmail.com>
2023-11-14 10:43:32 +0700

    Add main() to test DriverPresensi and DriverSimpeg

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
+from src.hrm.driver_presensi import DriverPresensi
+from src.hrm.driver_simpeg import DriverSimpeg
+from src.hrm.utils import get_username, get_password
 
+import time
+
+
+def main():
+    username = get_username()
+    password = get_password()
+
+    presensi = DriverPresensi(username, password)
+    presensi.login()
+    time.sleep(2)
+    presensi.quit()
+
+    time.sleep(2)
+
+    simpeg = DriverSimpeg(username, password)
+    simpeg.login()
+    time.sleep(2)
+    simpeg.quit()
+
+
+if __name__ == '__main__':
+    main()
