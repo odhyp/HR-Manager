aba7463b2731017bf6100be0de7cfcc3a3e5a35e
odhyp <pradhana.odhy@gmail.com>
2023-11-19 21:43:47 +0700

    Add name_format dict in get_name_format method

--- src/hrm/file_manager.py
+++ src/hrm/file_manager.py
@@ -1,4 +1,13 @@
 class FileManager:
+    def get_name_format(self, type):
+        name_format = {
+            "presensi": "Laporan Rekap Presensi",
+            "prestasi": "Laporan Rekap Prestasi",
+            "nominatif": "DAFTAR_PEGAWAI_NOMINATIF_",
+            "duk": "DAFTAR_URUT_KEPANGKATAN_",
+        }
+        return name_format[type]
+
     def is_xls(self, file_name):
         return file_name.endswith('.xls')
 
