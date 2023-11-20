18a10f8534e5018a2ead3b0015de4f1d01dbffdb
odhyp <pradhana.odhy@gmail.com>
2023-11-20 22:30:39 +0700

    Add test methods for main

--- main.py
+++ main.py
@@ -1,24 +1,29 @@
 from src.hrm.driver_presensi import DriverPresensi
 from src.hrm.driver_simpeg import DriverSimpeg
+from src.hrm.file_manager import FileManager
 from src.hrm.utils import get_username, get_password
-
-import time
 
 
 def main():
     username = get_username()
     password = get_password()
 
+    file_manager = FileManager()
+
     presensi = DriverPresensi(username, password)
     presensi.login()
-    time.sleep(2)
+    presensi.rekap_presensi("2023-11-01", "2023-11-20")
+    file_manager.wait_for_download('presensi')
+    presensi.rekap_prestasi("11/2023")
+    file_manager.wait_for_download('prestasi')
     presensi.quit()
-
-    time.sleep(2)
 
     simpeg = DriverSimpeg(username, password)
     simpeg.login()
-    time.sleep(2)
+    simpeg.nominatif()
+    file_manager.wait_for_download('nominatif')
+    simpeg.duk()
+    file_manager.wait_for_download('duk')
     simpeg.quit()
 
 
