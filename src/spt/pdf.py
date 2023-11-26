3215758166f3a81a6c795b4506a2ea48e4e9a787
odhyp <pradhana.odhy@gmail.com>
2023-11-26 22:30:16 +0700

    Fix text parameter using spt_texts dictionary

--- src/spt/pdf.py
+++ src/spt/pdf.py
@@ -1,6 +1,7 @@
 from fpdf import FPDF
 
 from src.spt import constants
+from src.spt.texts import spt_texts
 
 
 class PDF(FPDF):
@@ -35,7 +36,7 @@ class PDF(FPDF):
                       size=12)
         self.cell(w=0,
                   h=8,
-                  text="SURAT PERINTAH TUGAS",
+                  text=spt_texts['section_title'][0],
                   align="C",
                   new_x="LMARGIN",
                   new_y="NEXT",
@@ -46,7 +47,7 @@ class PDF(FPDF):
                       size=12)
         self.cell(w=0,
                   h=4,
-                  text=f"NOMOR : 800 / {letter_number}",
+                  text=f"{spt_texts['section_title'][1]}{letter_number}",
                   align="C",
                   new_x="LMARGIN",
                   new_y="NEXT",
@@ -58,7 +59,7 @@ class PDF(FPDF):
                       size=12)
         self.multi_cell(w=0,
                         h=8,
-                        text="Yang bertanda tangan di bawah ini Kepala Kantor Pelayanan Pajak Daerah DIY di Kabupaten Bantul BPKA DIY, dengan ini memerintahkan kepada:",
+                        text=spt_texts['section_title'][2],
                         new_x="LMARGIN",
                         new_y="NEXT",
                         border=constants.SHOW_BORDERS)
@@ -67,12 +68,6 @@ class PDF(FPDF):
     def section_employee(self):
         """Add employee data including Nama, NIP, Pangkat/Golongan, Jabatan.
         """
-        asn_name = ['Nama',
-                    'NIP',
-                    'Pangkat/Golongan',
-                    'Jabatan',
-                    ]
-
         asn_value = ['ODHY PRADHANA, A.Md.Ak.',
                      '199904192021021002',
                      'Pengatur, II/c',
@@ -89,7 +84,7 @@ class PDF(FPDF):
                             border=constants.SHOW_BORDERS)
             self.multi_cell(w=40,
                             h=6,
-                            text=asn_name[i],
+                            text=spt_texts['section_employee'][i],
                             new_x="RIGHT",
                             new_y="LAST",
                             border=constants.SHOW_BORDERS)
@@ -112,11 +107,6 @@ class PDF(FPDF):
     def section_assignment(self):
         """Add assignment data including Untuk, Hari/Tanggal, Tujuan Tugas.
         """
-        data_name = ['Untuk',
-                     'Hari/tanggal',
-                     'Tujuan Tugas',
-                     ]
-
         data_value = ['Rekonsiliasi Data dan Evaluasi Pendapatan Daerah Bulan Oktober Tahun 2023',
                       'Rabu, 8 November 2023',
                       'Badan Pengelola Keuangan dan Aset DIY Badan Pengelola Keuangan dan Aset DIY Badan Pengelola Keuangan dan Aset DIY',
@@ -125,7 +115,7 @@ class PDF(FPDF):
         for i in range(3):
             self.multi_cell(w=30,
                             h=8,
-                            text=data_name[i],
+                            text=spt_texts['section_assignment'][i],
                             new_x="RIGHT",
                             new_y="LAST",
                             border=constants.SHOW_BORDERS)
@@ -145,13 +135,10 @@ class PDF(FPDF):
 
         self.ln(8)
 
-    def section_date(self):
+    def section_date(self, letter_date):
         """Add letter date.
         """
-        tanggal_name = ['Dikeluarkan di Bantul',
-                        'Pada tanggal : ' + '26 November 2023',
-                        ]
-
+        spt_texts['section_date'][1] += letter_date
         for i in range(2):
             self.cell(w=60,
                       h=8,
@@ -160,7 +147,7 @@ class PDF(FPDF):
                       border=constants.SHOW_BORDERS)
             self.cell(w=0,
                       h=8,
-                      text=tanggal_name[i],
+                      text=spt_texts['section_date'][i],
                       new_x="RIGHT",
                       new_y="NEXT",
                       border=constants.SHOW_BORDERS)
@@ -180,7 +167,7 @@ class PDF(FPDF):
                   border=constants.SHOW_BORDERS)
         self.cell(w=0,
                   h=8,
-                  text='Plt. Kepala',
+                  text=spt_texts['section_signature'][0],
                   align='C',
                   new_x="RIGHT",
                   new_y="NEXT",
@@ -188,13 +175,10 @@ class PDF(FPDF):
 
         self.ln(24)
 
-        kepala_name = ['HIDAYATI YULIASTANTRI DJOHAR, S.Sos., M.Si.',
-                       'NIP. 19780723 200501 2 003']
-
         self.set_font(family='times',
                       size=12)
 
-        for i in range(2):
+        for i in range(1, 3):
             self.cell(w=60,
                       h=4,
                       new_x="RIGHT",
@@ -202,7 +186,7 @@ class PDF(FPDF):
                       border=constants.SHOW_BORDERS)
             self.cell(w=0,
                       h=4,
-                      text=kepala_name[i],
+                      text=spt_texts['section_signature'][i],
                       align='C',
                       new_x="RIGHT",
                       new_y="NEXT",
