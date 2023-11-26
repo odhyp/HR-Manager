2a6996af1571a705ee14fc343a49ea74b45d6967
odhyp <pradhana.odhy@gmail.com>
2023-11-26 20:41:06 +0700

    Update convert_to_csv function using getters

--- src/spt/duk.py
+++ src/spt/duk.py
@@ -1,17 +1,24 @@
 import pandas as pd
 
+from src.spt.utils import get_data_path, get_duk_path
+
 
 def convert_to_csv():
-    df = pd.DataFrame(pd.read_excel(io='duk.xlsx',
+    data_path = get_data_path()
+    duk_path = get_duk_path()
+    output_path = str(data_path) + '\\duk.txt'
+    df = pd.DataFrame(pd.read_excel(io=duk_path,
                                     index_col=None,
-                                    usecols=['JABATAN'],
-                                    # usecols=['NIP', 'NAMA', 'GOLONGAN', 'JABATAN'],
+                                    usecols=['NAMA',
+                                             'NIP',
+                                             'GOLONGAN',
+                                             'JABATAN'],
                                     skiprows=4,
                                     ))
 
     df['JABATAN'] = df['JABATAN'].str.replace('\n', ' ').str.replace('\r', '')
 
-    df.to_csv(path_or_buf='duk.txt',
+    df.to_csv(path_or_buf=output_path,
               sep=';',
               index=False,
               )
