ccce49b905b2f3c21e0d4c6f36a28cb1e77ac549
odhyp <pradhana.odhy@gmail.com>
2023-11-26 22:31:43 +0700

    Update main.py to accomodate testing

--- main.py
+++ main.py
@@ -2,15 +2,13 @@ from src.spt.pdf import PDF
 from src.spt import constants
 
 
-def main():
+def create_pdf():
     pdf = PDF('P', 'mm', constants.PAPER_SIZE)
     pdf.set_title('Surat Perintah Tugas')
     pdf.set_author('Odhy Pradhana')
-
-    pdf.print_chapter()
-
-    pdf.output('pdf_2.pdf')
+    pdf.print_section()
+    pdf.output('pdf_3.pdf')
 
 
 if __name__ == '__main__':
-    main()
+    create_pdf()
