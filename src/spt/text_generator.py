b510bf18d2f1f7fd0697349fb1735e7863bc553b
odhyp <pradhana.odhy@gmail.com>
2023-12-07 21:47:01 +0700

    Add TextGenerator to generate the required text for SPT

---
+++ src/spt/text_generator.py
@@ -0,0 +1,18 @@
+from src.spt.utils import get_base_text_path
+
+
+class TextGenerator:
+    base_text_path = get_base_text_path()
+    
+    def __init__(self):
+        self.text_list = []
+        self.read_txt_file()
+
+
+    def read_txt_file(self, file_path=base_text_path):
+        with open(file_path, 'r') as f:
+            for line in f:
+                self.text_list.append(line.rstrip('\n'))
+
+    def get_text(self):
+        return self.text_list
