65692c4d595891d724273693a5e5d752afcc929e
odhyp <pradhana.odhy@gmail.com>
2023-12-15 20:20:15 +0700

    Rewrite TextGenerator list generation

--- src/spt/text_generator.py
+++ src/spt/text_generator.py
@@ -2,17 +2,21 @@ from src.spt.utils import get_base_text_path, get_user_text_path
 
 
 class TextGenerator:
-    base_text_path = get_base_text_path()
-    
     def __init__(self):
-        self.text_list = []
-        self.read_txt_file()
+        self.base_text = []
+        self.user_text = []
+        self.populate_list('base_text')
+        self.populate_list('user_text')
 
 
-    def read_txt_file(self, file_path=base_text_path):
-        with open(file_path, 'r') as f:
-            for line in f:
-                self.text_list.append(line.rstrip('\n'))
-
-    def get_text(self):
-        return self.text_list
+    def populate_list(self, list_type):
+        if list_type == 'base_text':
+            with open(get_base_text_path(), 'r') as f:
+                for line in f:
+                    self.base_text.append(line.rstrip('\n'))
+        elif list_type == 'user_text':
+            with open(get_user_text_path(), 'r') as f:
+                for line in f:
+                    self.user_text.append(line.rstrip('\n'))
+        else:
+            return None
