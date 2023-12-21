fae3bae3730da37f7999283afb77040129c445b5
odhyp <pradhana.odhy@gmail.com>
2023-12-21 20:32:44 +0700

    Add constants values

---
+++ src/hrm/constants.py
@@ -0,0 +1,14 @@
+# Time wait before next action (seconds)
+# Mainly used in DriverSimpeg for working with dropdowns
+WAIT_TIME = 3.5
+
+# URLs
+URL_SIMPEG = "https://simpeg2.jogjaprov.go.id/prod/"
+URL_SIMPEG_NOMINATIF = f"{URL_SIMPEG}index.php/listing/nominatif"
+URL_SIMPEG_DUK = f"{URL_SIMPEG}index.php/duk"
+URL_PRESENSI = "https://presensi2.jogjaprov.go.id/"
+URL_PRESENSI_REKAP_PRESENSI = f"{URL_PRESENSI}lap-pres/rekap/?menu_id=15"
+URL_PRESENSI_REKAP_PRESTASI = f"{URL_PRESENSI}lap-pres/prestasi/?menu_id=16"
+
+# Organization Name
+NAMA_UNOR = "Kantor Pelayanan Pajak Daerah Daerah Istimewa Yogyakarta di Kabupaten Bantul"
