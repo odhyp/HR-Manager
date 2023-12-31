62b511f606c234f6cd379cc76232d3a21cd03cf6
odhyp <pradhana.odhy@gmail.com>
2023-12-31 18:46:36 +0700

    Merge constants.py and move to src

--- src/constants.py
+++ src/constants.py
@@ -1,6 +1,4 @@
-# Time wait before next action (seconds)
-# Mainly used in DriverSimpeg for working with dropdowns
-WAIT_TIME = 3.5
+#----------# Webdriver Constants #----------#
 
 # URLs
 URL_SIMPEG = "https://simpeg2.jogjaprov.go.id/prod/"
@@ -12,3 +10,22 @@ URL_PRESENSI_REKAP_PRESTASI = f"{URL_PRESENSI}lap-pres/prestasi/?menu_id=16"
 
 # Organization Name
 NAMA_UNOR = "Kantor Pelayanan Pajak Daerah Daerah Istimewa Yogyakarta di Kabupaten Bantul"
+
+# Time wait before next action (seconds)
+# Mainly used in DriverSimpeg for working with dropdowns
+WAIT_TIME = 3.5
+
+
+#----------# PDF Constants #----------#
+
+# Show cell borders (used for testing)
+SHOW_BORDERS = False
+
+# Page size (mm). Default is F4 paper
+PAPER_SIZE = (215, 330)
+
+# Page margins (mm). Top, bottom, left, and right margin
+PAPER_MARGINS = 25.4
+
+# Font size. Default is 12
+FONT_SIZE = 12
